c2b56f5ab567ad29668d4914cbcb8c38bdc35984
Viktor Romanyuk <viktoraromanyuk@gmail.com>
2021-12-21 15:00:08 +0200

    updated eslint, updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'element-plus',
-        // import component
-        customName: (name) => {
-          name = name.slice(3)
-          return `element-plus/lib/components/${name}`
-        }
-      }
-    ]
   ]
 }
