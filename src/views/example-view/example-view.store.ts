fc23c9f1a9574aac834a9c0ed385d3e8babf9e85
Viktor Romanyuk <viktoraromanyuk@gmail.com>
2022-04-29 11:35:47 +0300

    fixed element styles

--- src/views/example-view/example-view.store.ts
+++ src/views/example-view/example-view.store.ts
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { IExampleInterface, TNullableField } from '@/types'
 import { exampleViewService } from '@/services'
 
 // the first argument is a unique id of the store across your application
 export const useExampleViewStore = defineStore('exampleViewStore', () => {
   const exampleVar = ref<TNullableField<IExampleInterface>>(null)
+
+  const getter = computed(() => exampleVar.value)
 
   function setStaticData () {
     exampleVar.value = {
@@ -19,6 +21,7 @@ export const useExampleViewStore = defineStore('exampleViewStore', () => {
   }
 
   return {
+    getter,
     exampleVar,
     setStaticData,
     getTestVar
