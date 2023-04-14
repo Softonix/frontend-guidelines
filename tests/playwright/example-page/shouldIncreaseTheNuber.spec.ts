fe091c2cc1ed8a3211f7dc7805b5796a9871434d
admin <admin@admins-MacBook-Pro.local>
2023-04-14 11:48:32 +0300

    increasing number test added

--- tests/playwright/example-page/shouldIncreaseTheNuber.spec.ts
+++ tests/playwright/example-page/shouldIncreaseTheNuber.spec.ts
@@ -1,19 +1,31 @@
 import { test, expect } from '@playwright/test'
 
-test.only('Clicking the [] button should respond with incremented id', async ({ page, request }) => {
+test.only('Clicking the [] button should respond with incremented id', async ({ page }) => {
+  let responseId
+  const generalStoreBlock = page.getByTestId('general-store-text')
   await page.goto('http://localhost:5173/example')
 
-  const catchFirstRequest = page.waitForResponse(resp => resp.url().includes('https://jsonplaceholder.typicode.com/posts/'))
+  const firstRequest = page.waitForRequest(request => request.url().includes('https://jsonplaceholder.typicode.com/posts/'))
 
   await page.getByTestId('general-store-button').click()
 
-  const firstResponce = await catchFirstRequest.json()
-  console.log(firstResponce.id)
+  await (await firstRequest).response().then(async (response) => {
+    await response?.json().then((resp) => {
+      responseId = resp.id
+    })
+  })
 
-  const catchSecondRequest = page.waitForResponse(resp => resp.url().includes('https://jsonplaceholder.typicode.com/posts/'))
+  await expect(generalStoreBlock).toContainText(`"id": ${responseId}`)
+
+  const secondRequest = page.waitForRequest(request => request.url().includes('https://jsonplaceholder.typicode.com/posts/'))
 
   await page.getByTestId('general-store-button').click()
 
-  const secondResponce = await catchSecondRequest.json()
-  console.log(secondResponce.id)
+  await (await secondRequest).response().then(async (response) => {
+    await response?.json().then((resp) => {
+      responseId = resp.id
+    })
+  })
+
+  await expect(generalStoreBlock).toContainText(`"id": ${responseId}`)
 })
