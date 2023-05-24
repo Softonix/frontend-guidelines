2869ec42ae8ca55c4e31571c8d8e43e636edc8e5
Oleh <my.mail.olech@gmail.com>
2023-05-25 00:04:50 +0300

    fpc-16: added guard for chat, added chat/:id route

--- src/views/chat/chat.routes.ts
+++ src/views/chat/chat.routes.ts
@@ -1,13 +1,37 @@
-import type { RouteRecordRaw } from 'vue-router'
+import type { NavigationGuardNext, RouteLocationNormalized, RouteRecordRaw } from 'vue-router'
 
 export const chatRouteNames = {
-  chat: 'chat'
+  chat: 'chat',
+  chatRoom: 'chatRoom'
+}
+
+export const chatRouteGuard = async (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  // TODO: Implement load last chat or group
+  const lastChatId = 1
+  if (lastChatId) {
+    next(`/chat/${lastChatId}`)
+  } else {
+    next()
+  }
 }
 
 export const chatRoutes: Array<RouteRecordRaw> = [
   {
-    path: '/chat',
+    path: '/chat/',
     name: chatRouteNames.chat,
+    component: () => import('./Chat.vue'),
+    meta: {
+      requireAuth: true
+    },
+    beforeEnter: chatRouteGuard
+  },
+  {
+    path: '/chat/:id',
+    name: chatRouteNames.chatRoom,
     component: () => import('./Chat.vue'),
     meta: {
       requireAuth: true
