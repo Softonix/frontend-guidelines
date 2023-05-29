2c27ad25527ac7f923ba131e2aa5392b043c49c3
Oleh <my.mail.olech@gmail.com>
2023-05-29 19:32:40 +0300

    fpc-18: refactor: optimized data fetching, feat: load last chat's messages by default

--- src/views/chat/Chat.vue
+++ src/views/chat/Chat.vue
@@ -2,14 +2,16 @@
   <div class="flex flex-col justify-items-center items-center pt-2 pb-2 md:pb-5 overflow-hidden h-full">
     <div class="overflow-y-auto no-scrollbar flex-1 w-full px-5 md:px-20 pb-5 flex flex-col gap-6">
       <Message
-        v-for="message in messages" :key="message.id"
-        :message="message"
+        v-for="message in messages" :key="message.id" :message="message"
         :currentUserMessage="currentUser?.id === message.sender_id"
       />
     </div>
 
     <div class="md:min-w-[320px] w-full pt-2 px-5 flex-shrink-0 pb-5">
-      <MessageForm :chatId="chatId" :senderId="senderId" />
+      <MessageForm
+        :chatId="($route.params.id as string)"
+        :senderId="currentUser?.id || null"
+      />
     </div>
   </div>
 </template>
@@ -22,9 +24,8 @@ import MessageForm from './components/MessageForm.vue'
 
 const chatStore = useChatStore()
 const authStore = useAuthStore()
-const chatId = ref<string | null>(null)
-const senderId = ref<string | null>(null)
 const { currentUser } = storeToRefs(authStore)
+
 const { messages } = storeToRefs(chatStore)
 
 const chatRoomGuard = async (
