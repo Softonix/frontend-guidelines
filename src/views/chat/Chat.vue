9ffd9f86511be322547fa51574dee80ad3ddd16c
Oleh <my.mail.olech@gmail.com>
2023-06-01 12:43:04 +0300

    fpc-19: refactor

--- src/views/chat/Chat.vue
+++ src/views/chat/Chat.vue
@@ -3,7 +3,6 @@
     class="flex flex-col justify-items-center items-center pt-2 pb-2 md:pb-5 overflow-hidden h-full"
   >
     <div
-      ref="messageListRef"
       class="overflow-y-auto no-scrollbar flex-1 w-full px-5 md:px-20 pb-5 flex flex-col gap-6"
     >
       <Message
@@ -28,8 +27,6 @@
 <script lang="ts" setup>
 import Message from './components/Message.vue'
 import MessageForm from './components/MessageForm.vue'
-
-const messageListRef = ref<HTMLDivElement | null>(null)
 
 const route = useRoute()
 
