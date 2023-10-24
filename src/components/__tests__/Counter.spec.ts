c0c5cfedd3efe29ca04d61edc22a04068720b1ad
Artur Dorfman <a.dorfma@softonix.org>
2023-10-24 12:17:50 +0300

    updated test in Counter.spec.ts

--- src/components/__tests__/Counter.spec.ts
+++ src/components/__tests__/Counter.spec.ts
@@ -1,20 +1,20 @@
-import { test } from 'vitest'
-import { render, fireEvent, screen } from '@testing-library/vue'
+import { it } from 'vitest'
+import { render, fireEvent } from '@testing-library/vue'
 import Counter from '../Counter.vue'
 
-test('increments value on click', async () => {
-  render(Counter)
+it('should increments value on click', async () => {
+  const { getByText } = render(Counter)
 
   // screen has all queries that you can use in your tests.
   // getByText returns the first matching node for the provided text, and
   // throws an error if no elements match or if more than one match is found.
-  screen.getByText('Times clicked: 0')
+  getByText('Times clicked: 0')
 
-  const button = screen.getByText('increment')
+  const button = getByText('increment')
 
   // Dispatch a native click event to our button element.
   await fireEvent.click(button)
   await fireEvent.click(button)
 
-  screen.getByText('Times clicked: 2')
+  getByText('Times clicked: 2')
 })
