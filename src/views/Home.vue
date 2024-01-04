e4852e20876cd7cb1e8c1d4e9060c2672a6a57bb
Viktor Romanyuk <v.romanyuk@softonix.org>
2024-01-04 17:21:03 +0200

    vite 5, vue 3.4 migration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Hello home</div>
+  <div class="p-5" :style="{background: colors.orange[400]}">{{ testVar }}</div>
 </template>
+
+<script lang="ts" setup>
+import { colors } from '@colors'
+
+const generalStore = useGeneralStore()
+const { testVar } = storeToRefs(generalStore)
+</script>
