dd592c7d5f9cc9693526c4b3510c26d69aa41189
Viktor Romanyuk <v.romanyuk@softonix.org>
2024-01-16 17:33:01 +0200

    fixed checkbox

--- src/views/example-view/custom-checkbox/components/CustomCheckbox.vue
+++ src/views/example-view/custom-checkbox/components/CustomCheckbox.vue
@@ -1,34 +1,30 @@
 <template>
-  <Compute #default="{isChecked}" isChecked="[&:has(input:checked)]">
-    <label
-      v-bind="$attrs"
+  <label
+    :class="[
+      'rounded-lg bg-white relative inline-block shadow-md overflow-hidden rounded-4xl cursor-pointer',
+      'group ring-1 ring-gray-300 [&:has(input:checked)]:ring-primary'
+    ]"
+  >
+    <input v-model="modelValue" class="hidden" type="checkbox" :value="value">
+    <IconCheckFilled
       :class="[
-        'rounded-lg bg-white relative inline-block shadow-md overflow-hidden rounded-4xl cursor-pointer',
-        `group ring-1 ring-gray-300 ${isChecked}:ring-primary`
+        `transition-opacity opacity-0 group-[&:has(input:checked)]:opacity-100`,
+        'text-primary absolute right-1 top-1 z-10'
       ]"
-    >
-      <input v-model="modelValue" class="hidden" type="checkbox" :value="value">
-      <IconCheckFilled
-        :class="[
-          `transition-opacity opacity-0 group-${isChecked}:opacity-100`,
-          'text-primary absolute right-1 top-1 z-10'
-        ]"
-      />
+    />
 
-      <div class="bg-gray-200 border-b border-gray-300">
-        <img :src="data.img" alt="checkbox-image">
-      </div>
+    <div class="bg-gray-200 border-b border-gray-300">
+      <img :src="data.img" alt="checkbox-image">
+    </div>
 
-      <div class="p-4">
-        <p class="font-bold">{{ data.title }}</p>
-        <p class="text-gray-500">{{ data.description }}</p>
-      </div>
-    </label>
-  </Compute>
+    <div class="p-4">
+      <p class="font-bold">{{ data.title }}</p>
+      <p class="text-gray-500">{{ data.description }}</p>
+    </div>
+  </label>
 </template>
 
 <script lang="ts" setup>
-defineOptions({ inheritAttrs: false })
 
 defineProps<{
   value?: any
